0914d4a873b3630fcbcf65b787b0c1aff2799898
Archana <archanab1894@gmail.com>
2019-01-19 23:01:40 +0530

    [Archana] - Fix test cases

--- spec/support/movie_attributes.rb
+++ spec/support/movie_attributes.rb
@@ -3,7 +3,7 @@ def movie_attributes(overrides = {})
 		name: "Movie 1",
 		description: "ok" ,
 		ratings: 9,
-		show_time: DateTime.now,
+		show_time: 10.days.since,
 		price: 10
 	}.merge(overrides)
 end
